744298535dd1e75896050bbafc26cc4c28d8945a
hongs0312 <hongseongho300@gmail.com>
2026-09-01 00:27:06 +0900

    fix(cache): 캐시 갱신 함수 불필요 인자 제거

--- src/cache.rs
+++ src/cache.rs
@@ -6,22 +6,21 @@ cache.rs
 use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 
-use serenity::all::{ChannelType, UserId};
+use serenity::all::UserId;
 use serenity::gateway::ShardManager;
-use serenity::http::Http;
-use serenity::model::id::GuildId;
 use serenity::prelude::TypeMapKey;
 
 use tokio::sync::{mpsc, RwLock};
 
 use crate::integration::notion::member::NotionMember;
-use crate::integration::notion::project::{get_projects, Project, Status};
+use crate::integration::notion::project::{get_projects, Project};
 
 //봇이 전체적으로 공유할 캐쉬 구조체
 pub struct BotCache {
     // 유저 아이디로 관리
     pub all_members: HashMap<UserId, String>,
-    pub project_mapping: Vec<Project>,
+    pub project_mapping: HashMap<String, usize>, // 프로젝트 이름 -> project_vec 인덱스
+    pub project_vec: Vec<Project>,
 }
 
 // 캐시 스레드가 처리할 명령 목록
@@ -64,8 +63,6 @@ impl TypeMapKey for ShardManagerContainer {
 // 쓰레드 구성
 pub fn start_cache_thread(
     cache: Arc<RwLock<BotCache>>,
-    http: Arc<Http>,
-    guild_id: GuildId,
 ) -> mpsc::Sender<CacheCommand> {
     // 버퍼 크기가 32인 비동기 채널 생성(가동신호 수신용)
     let (tx, mut rx) = mpsc::channel::<CacheCommand>(32);
@@ -74,13 +71,13 @@ pub fn start_cache_thread(
         println!("백그라운드 동기화 스레드 가동");
 
         // 봇이 켜졌을떄 한 번 연동
-        refresh_cache(&cache, &http, guild_id).await;
+        refresh_cache(&cache).await;
 
         while let Some(command) = rx.recv().await {
             match command {
                 CacheCommand::RefreshAll => {
                     println!("[캐시] 전체 캐시 즉시 동기화 요청 처리 중...");
-                    refresh_cache(&cache, &http, guild_id).await;
+                    refresh_cache(&cache).await;
                 }
                 CacheCommand::UpdateSingleMember {
                     user_id,
@@ -113,75 +110,36 @@ pub fn start_cache_thread(
 }
 
 // 캐쉬 갱신 함수
-async fn refresh_cache(cache: &Arc<RwLock<BotCache>>, http: &Arc<Http>, guild_id: GuildId) {
-    if let Ok(members) = guild_id.members(&http, None, None).await {
-        if let Ok(server_roles) = guild_id.roles(&http).await {
-            let mut new_cache = BotCache {
-                all_members: HashMap::new(),
-                project_mapping: Vec::new(),
-            };
-            // 옛날 코드
-            // // 맴버 별로 순회하면서 해당 프로젝트에 참여중인지 아닌지 확인
-            // for member in members {
-            //     let user_id = member.user.id; // 💡 유저 고유 ID 추출
-            //     let username = member.display_name(); // 서버에 표시되는 이름 추출
+async fn refresh_cache(cache: &Arc<RwLock<BotCache>>) {
+    let mut new_cache = BotCache {
+        all_members: HashMap::new(),
+        project_mapping: HashMap::new(),
+        project_vec: Vec::new(),
+    };
 
-            //     new_cache.all_members.insert(user_id, username.to_string());
-
-            //     //맴버가 가진 역할과 프로젝트명 비교
-            //     for role_id in &member.roles {
-            //         // 포함된 프로젝트에 매핑
-            //         if let Some(role) = server_roles.get(role_id) {
-            //             new_cache
-            //                 .project_mapping
-            //                 .entry(role.name.clone())
-            //                 .or_insert_with(HashSet::new)
-            //                 .insert(user_id);
-            //         }
-            //     }
-            // }
-            // // 프로젝트 목록을 받아와서 PM추출하기
-            // if let Ok(channels) = guild_id.channels(&http).await {
-            //     for (_channel_id, channel) in channels {
-            //         // 카테고리(프로젝트)이면서 이름에 "(PM: " 택스트가 붙어 있는지 필터링
-            //         if channel.kind == ChannelType::Category && channel.name.contains("(PM: ") {
-            //             //프로젝트명과 pm이름 분리
-            //             let parts: Vec<&str> = channel.name.split("(PM: ").collect();
-            //             if parts.len() == 2 {
-            //                 let project_name = parts[0].to_string();
-            //                 let pm_name = parts[1].trim_end_matches(')');
-
-            //                 // pm이름을 이용해 user_id 역추척하기
-            //                 if let Some((&pm_id, _)) = new_cache
-            //                     .all_members
-            //                     .iter()
-            //                     .find(|(_, name)| **name == pm_name)
-            //                 {
-            //                     new_cache.project_pms.insert(project_name, pm_id);
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
-
-            match get_projects().await {
-                Ok(projects) => {
-                    new_cache.project_mapping = projects;
-                }
-                Err(e) => {
-                    eprintln!("프로젝트 목록을 가져오는 데 실패했습니다: {}", e);
-                }
+    match get_projects().await {
+        Ok(projects) => {
+            new_cache.project_vec = projects;
+            // 프로젝트 이름을 키로 사용하여 인덱스를 매핑
+            for (index, project) in new_cache.project_vec.iter().enumerate() {
+                new_cache
+                    .project_mapping
+                    .insert(project.name.clone(), index);
             }
-
-            //새로 갱신한 값 덮어쓰기
-            {
-                let mut lock = cache.write().await;
-                *lock = new_cache;
-            }
-            println!("백그라운드 데이터 갱신 완료");
+        }
+        Err(e) => {
+            eprintln!("프로젝트 목록을 가져오는 데 실패했습니다: {}", e);
         }
     }
+
+    //새로 갱신한 값 덮어쓰기
+    {
+        let mut lock = cache.write().await;
+        *lock = new_cache;
+    }
+    println!("백그라운드 데이터 갱신 완료");
 }
+
 
 // 단일 유저 캐시 갱신
 async fn update_single_member(
