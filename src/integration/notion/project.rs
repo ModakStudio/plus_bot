10975eb2376c099c365b37c91ca9549238023658
JiSeong Lee <cocoquiet@knu.ac.kr>
2026-09-02 12:51:56 +0900

    Feat: Add field(category_id) in struct(Project)

--- src/integration/notion/project.rs
+++ src/integration/notion/project.rs
@@ -34,6 +34,7 @@ pub struct Project {
     pub github: String,
     pub pm: Member,
     pub participants: Vec<Member>,
+    pub category_id: String,
 }
 
 impl Project {
@@ -76,6 +77,10 @@ impl Project {
             .into_iter()
             .map(|m| m.unwrap_or_default())
             .collect(),
+            category_id: properties["category_id"]["rich_text"][0]["text"]["content"]
+                .as_str()
+                .unwrap_or_default()
+                .to_string(),
         }
     }
 }
